437712bb9a08211f36ace649c4080ff29f26cac2
Graham Wihlidal <graham@wihlidal.ca>
2018-12-04 16:07:54 +0100

    Added clusterize docs

--- src/clusterize.rs
+++ src/clusterize.rs
@@ -4,6 +4,10 @@ use DecodePosition;
 pub type Bounds = ffi::meshopt_Bounds;
 pub type Meshlet = ffi::meshopt_Meshlet;
 
+/// Splits the mesh into a set of meshlets where each meshlet has a micro index buffer indexing into meshlet vertices that refer to the original vertex buffer
+/// The resulting data can be used to render meshes using NVidia programmable mesh shading pipeline, or in other cluster-based renderers.
+/// For maximum efficiency the index buffer being converted has to be optimized for vertex cache first.
+/// Note: `max_vertices` must be <= 64 and `max_triangles` must be <= 126
 pub fn build_meshlets(indices: &[u32], vertex_count: usize, max_vertices: usize, max_triangles: usize) -> Vec<Meshlet> {
     let meshlet_count = unsafe {
         ffi::meshopt_buildMeshletsBound(indices.len(), max_vertices, max_triangles)
@@ -16,6 +20,23 @@ pub fn build_meshlets(indices: &[u32], vertex_count: usize, max_vertices: usize,
     meshlets
 }
 
+/// Creates bounding volumes that can be used for frustum, backface and occlusion culling.
+///
+/// For backface culling with orthographic projection, use the following formula to reject backfacing clusters:
+///   `dot(view, cone_axis) >= cone_cutoff`
+///
+/// For perspective projection, you can the formula that needs cone apex in addition to axis & cutoff:
+///   `dot(normalize(cone_apex - camera_position), cone_axis) >= cone_cutoff`
+///
+/// Alternatively, you can use the formula that doesn't need cone apex and uses bounding sphere instead:
+///   `dot(normalize(center - camera_position), cone_axis) >= cone_cutoff + radius / length(center - camera_position)`
+/// or an equivalent formula that doesn't have a singularity at center = camera_position:
+///   `dot(center - camera_position, cone_axis) >= cone_cutoff * length(center - camera_position) + radius`
+///
+/// The formula that uses the apex is slightly more accurate but needs the apex; if you are already using bounding sphere
+/// to do frustum/occlusion culling, the formula that doesn't use the apex may be preferable.
+///
+/// `index_count` should be <= 256*3 (the function assumes clusters of limited size)
 pub fn compute_cluster_bounds<T: DecodePosition>(indices: &[u32], vertices: &[T]) -> Bounds {
     let vertices = vertices
         .iter()
